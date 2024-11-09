1ac73a248997af4746ea90c679794bf445a81787
24PBL <ijshthat24@gmail.com>
2024-11-09 18:04:24 +0900

    22

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.post('/login', (req, res) => {
         }
 
         // JWT 생성
-        const token = jwt.sign({ id: user.id }, 'yourSecretKey', { expiresIn: '1h' });
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
 
         console.log("JWT Token:", token);
 
